1925ee14ad03f497b5a796cf416ed69101f16813
lijiacong <362598868@qq.com>
2015-05-17 13:46:10 +0800

    finish lesson 9

--- index.js
+++ index.js
@@ -1,10 +1,76 @@
 var http = require('http');
 
 var myexpress = function() {
-	return http.createServer(function(req, res) {
-		res.statusCode = 404;
+	var app = function(req, res) {
+		var next = function (err) {
+			var m;
+			if(err) {
+				m = app.stack.shift();
+				//如果是App embedding as middleware
+				if(m != undefined && m.hasOwnProperty('use')) {
+					m = m.stack.shift();
+					//寻找subApp中的第一个error handler
+					while(m != undefined && m.length != 4) {
+						m = m.shift();
+					}
+					if(m != undefined) {
+						m(err, req, res, next);
+					}
+					m = app.stack.shift();
+				}
+				while(m != undefined && m.length != 4) {
+					m = app.stack.shift();
+				}
+				if(m == undefined) {
+					res.statusCode=500;
+					return err;
+				}
+				 try{
+				 	m(err, req, res, next);
+				 } catch(ex) {
+				 	next(ex);
+				 }
+			} else {
+				m = app.stack.shift();
+				//如果是App embedding as middleware
+				if(m != undefined && m.hasOwnProperty('use')) {
+					m = m.stack.shift();
+					//寻找subApp中的第一个非error handler
+					while(m != undefined && m.length == 4) {
+						m = m.shift();
+					}
+					if(m != undefined) {
+						m(req, res, next);
+					}
+					m = app.stack.shift();
+				}
+				while(m != undefined && m.length == 4) {
+					m = app.stack.shift();
+				}
+				if(m == undefined) {
+					res.statusCode = 404;
+					return;
+				}
+				try{
+					m(req, res, next);
+				} catch(e) {
+					next(e);
+				}
+			}
+		}
+		next();
 		res.end();
-	});
+	};
+	app.listen = function(port, done) {
+		var server = http.createServer(app);
+		server.listen(port, done);
+		return server;
+	}
+	app.stack = [];
+	app.use = function(m) {
+		app.stack.push(m);
+	};
+	return app;
 }
 
 module.exports = myexpress;
--- test/app_spec.js
+++ test/app_spec.js
@@ -32,4 +32,163 @@ describe('app', function() {
 			});
 		});
 	});
+
+	describe('.use', function() {
+		describe('Implement app.use', function() {
+			var app = express();
+			var m1 = function() {};
+			var m2 = function() {};
+			app.use(m1);
+			app.use(m2);
+			it('should be able to add middlewares to stack', function() {
+				expect(app.stack[0]).to.equal(m1);
+				expect(app.stack[1]).to.equal(m2);
+			});	
+		});
+	});
+
+	describe('calling middleware stack', function() {
+		var app;
+		beforeEach(function() {
+			app = new express();
+		});
+
+		it('Should be able to call a single middleware', function(done) {
+			var m1 = function(req, res, next) {
+				res.end('hello from m1');
+			};
+			app.use(m1);
+			request(app).get('/').expect('hello from m1').end(done);
+		});
+
+		it('Should be able to call next to go to the next middleware', function(done) {
+			var m1 = function(req, res, next) {
+				next();
+			};
+
+			var m2 = function(req, res, next) {
+				res.end('hello from m2');
+			};
+
+			app.use(m1);
+			app.use(m2);
+			request(app).get('/').expect('hello from m2').end(done);
+		});
+
+		it('Should 404 at the end of middleware chain', function(done) {
+			var m1 = function(req, res, next) {
+				next();
+			};
+
+			var m2 = function(req, res, next) {
+				next();	
+			};
+
+			app.use(m1);
+			app.use(m2);
+			request(app).get('/').expect(404).end(done);			
+		});
+
+		it('Should 404 if no middleware is added', function(done) {
+			app.stack = [];
+			request(app).get('/').expect(404).end(done);
+		});
+	});
+
+	describe('Error handle', function() {
+		var app;
+		beforeEach(function() {
+			app = new express();
+		});
+
+		it('should return 500 for unhandled error', function(done) {
+			var m1 = function(req, res, next) {
+				next(new Error('boom!'));
+			}
+			app.use(m1);
+			request(app).get('/').expect(500).end(done);
+		});
+
+		it('should return 500 for uncaught error', function(done) {
+			var m1 = function(req, res, next) {
+				throw new Error('boom!');
+			};
+			app.use(m1);
+			request(app).get('/').expect(500).end(done);
+		});		
+
+		it('should skip error handlers when next is called without an error', function(done) {
+			var m1 = function(req, res, next) {
+				next();
+			};
+
+			var e1 = function(err, req, res, next) {
+
+			};
+
+			var m2 = function(req, res, next) {
+				res.end('m2');
+			};
+
+			app.use(m1);
+			app.use(e1);
+			app.use(m2);
+			request(app).get('/').expect('m2').end(done);
+		});
+
+		it('should skip normal middleware if next is called with an error', function(done) {
+			var m1 = function(req, res, next) {
+				next(new Error('boom!'));
+			};
+
+			var m2 = function(req, res, next) {
+
+			};
+
+			var e1 = function(err, req, res, next) {
+				res.end('e1');
+			};
+
+			app.use(m1);
+			app.use(m2);
+			app.use(e1);
+
+			request(app).get('/').expect('e1').end(done);
+		});
+	});
+
+	describe('App embedding as middleware', function() {
+		var app;
+		var subApp;
+		beforeEach(function() {
+			app = new express();
+			subApp = new express();
+		});
+
+		it('should pass unhandled request to parent', function(done) {
+			function m2(req, res, next) {
+				res.end('m2');
+			}
+			app.use(subApp);
+			app.use(m2);
+			request(app).get('/').expect('m2').end(done);
+		});
+
+		it('should pass unhandled error to parent', function(done) {
+			function m1(req, res, next) {
+				next('m1 error');
+			}
+
+			function e1(err, req, res, next) {
+				res.end(err);
+			}
+
+			subApp.use(m1);
+			app.use(subApp);
+			app.use(e1);
+			request(app).get('/').expect('m1 error').end(done);
+		});
+
+
+	});
 });
